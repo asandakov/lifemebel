da9c3ffd4d2dcbaed5112cc6c40dd2c0d8a0e9fc
asandakov <66016536+asandakov@users.noreply.github.com>
2021-02-14 15:27:51 +0300

    правка

--- lifemebel/ajax/ajax.php
+++ lifemebel/ajax/ajax.php
@@ -1,7 +1,7 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <?
-
+//
 global $DB,$USER,$APPLICATION;
 CBitrixComponent::includeComponentClass("site:lifemebel");
 
